b2e036b407fad1010b2e54a3faba4994c9a97bbc
Kirimoshi <baronaxaro@gmail.com>
2023-03-13 11:10:45 +0200

    [1.1:13.03]: finished application form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
+import CandidateAddForm from "./components/candidate-add-form/candidate-add-form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const inputs = [
+  {type: "text", name: "name", id: "name", placeholder: "Имя", label: "Имя", },
+  {type: "text", name: "surname", id: "surname", placeholder: "Фамилия", label: "Фамилия", },
+  {type: "date", name: "dateOfBirth", id: "dateOfBirth", placeholder: "Дата рождения", label: "Дата рождения", },
+  {type: "tel", name: "phoneNumber", id: "phoneNumber", placeholder: "Телефон", label: "Телефон", },
+  {type: "url", name: "websiteURL", id: "websiteURL", placeholder: "Сайт", label: "Сайт", },
+]
+const textareas = [
+  {name: "about", id: "about", placeholder: "О себе", label: "О себе", },
+  {name: "techStack", id: "techStack", placeholder: "Стек технологий", label: "Стек технологий", },
+  {name: "lastProjectDescription", id: "lastProjectDescription", placeholder: "Описание последнего проекта",
+    label: "Описание последнего проекта", },
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <CandidateAddForm inputs={ inputs } textareas={ textareas } />
+      </div>
+    )
+  }
 }
 
 export default App;
